e72a6aaa9bd0294fb1b5ae3a37d4e3b61d8b0422
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2021-04-12 15:48:57 -0300

    correcting minor dir-locals errors

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((magit-status-mode
-  (magit-todos-exclude-globs "doctest\.h" "*doc/*")))
+  (magit-todos-exclude-globs "doc/*" "doctest.h")))
